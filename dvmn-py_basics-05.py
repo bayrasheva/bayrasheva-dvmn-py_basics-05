8dc20fe579da2ceaa8ed6f7cb0a4a08147588afd
bayrasheva <angelinabairasheva@yandex.com>
2025-10-08 00:32:05 +0300

    Add files via upload

---
+++ dvmn-py_basics-05.py
@@ -0,0 +1,80 @@
+import file_operations
+import random
+import os
+from faker import Faker
+
+
+FAKE = Faker("ru_RU")
+
+SKILLS = ["Стремительный прыжок",
+"Электрический выстрел",
+"Ледяной удар",
+"Стремительный удар", 
+"Кислотный взгляд",
+"Тайный побег",
+"Ледяной выстрел",
+"Огненный заряд"]
+
+LETTERS_MAPPING = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+    'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+    'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+    'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+    'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+    'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+    'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+    'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+    'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+    'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+    }
+    
+def runic_alphabet_skills():
+	runic_skills = []	
+	for skill in SKILLS:
+		runic_skill = skill
+		for letter in LETTERS_MAPPING:
+			runic_skill = runic_skill.replace(letter, LETTERS_MAPPING[letter])
+		runic_skills.append(runic_skill) 
+	return runic_skills
+
+def main():
+	os.makedirs("output/svg", exist_ok=True)
+	runic_skills = runic_alphabet_skills()
+	for number in range(10):
+		selected_skill = random.sample(runic_skills, 3)
+		skill_1 = selected_skill[0]
+		skill_2 = selected_skill[1]
+		skill_3 = selected_skill[2]
+
+		context = {
+            "first_name": FAKE.first_name(),
+            "last_name": FAKE.last_name(),
+            "job": FAKE.job(),
+            "town": FAKE.city(),
+            "strength": random.randint(3, 18),
+            "agility": random.randint(3, 18),
+            "endurance": random.randint(3, 18),
+            "intelligence": random.randint(3, 18),
+            "luck": random.randint(3, 18),
+            "skill_1": skill_1,
+            "skill_2": skill_2,
+            "skill_3": skill_3
+        }
+
+		file_operations.render_template("charsheet.svg", f"output/svg/result_{number}.svg", context)
+
+if __name__ == "__main__":
+	main()
